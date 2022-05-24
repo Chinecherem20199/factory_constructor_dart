cef517d2c4215952bcc8580dbf058d1c3641c0e4
Chinecherem Augustina Ugwuanyi <51314161+Chinecherem20199@users.noreply.github.com>
2022-05-24 14:16:41 +0100

    Add files via upload
    Factory Constructor in dart

---
+++ factory_constructor.dart
@@ -0,0 +1,98 @@
+class Reading {
+  double value;
+  Reading({required double value}) : this.value = value;
+
+  Reading.zero() : value = -999;
+
+  @override
+  String toString() {
+    return '$value';
+  }
+
+  //Create factory constructor
+  //Note you must use factory keyword in order to make it factory constructor
+  factory Reading.fromServer(Map<String, Object> server) {
+    final type = server['type'];
+    final reading = server['reading'];
+    switch (type) {
+      case 'temp':
+        final city = server['city'];
+        if (city is String && reading is double) {
+          return Temperature(city: city, reading: reading);
+        } else {
+          Temperature.zero();
+        }
+        break;
+
+      case 'pressure':
+        final object = server['object'];
+        if (object is String && reading is double) {
+          return Pressure(objectTested: object, reading: reading);
+        } else {
+          Pressure.zero();
+        }
+    }
+    return Reading.zero();
+  }
+}
+
+class Temperature extends Reading {
+  String city;
+  Temperature({required String city, required double reading})
+      : this.city = city,
+        super(value: reading);
+  Temperature.zero()
+      : city = '',
+        super(value: -999);
+
+  @override
+  String toString() {
+    return 'Temperature reading:\nReading: ${super.toString()}\nCity: $city \n';
+  }
+}
+
+class Pressure extends Reading {
+  String objectTested;
+  Pressure({required String objectTested, required double reading})
+      : this.objectTested = objectTested,
+        super(value: reading);
+  Pressure.zero()
+      : objectTested = '',
+        super(value: -999);
+  @override
+  String toString() {
+    return 'Pressure reading:\nReading: ${super.toString()}\nObject Tested: $objectTested \n';
+  }
+}
+
+void main() {
+  var fromServer = [
+    {
+      'type': 'temp',
+      'reading': 20.2,
+      'city': 'Johannesburg',
+    },
+    {
+      'type': 'pressure',
+      'reading': 100.0,
+      'object': 'Gass cilinder',
+    },
+    {
+      'type': 'temp',
+      'reading': 35.2,
+      'city': 'New York',
+    },
+    {
+      'type': 'pressur',
+      'reading': 300.5,
+      'object': 'Tyre',
+    }
+  ];
+  var readings = <Reading>[];
+  for (var item in fromServer) {
+    readings.add(Reading.fromServer(item));
+  }
+  for (var reading in readings) {
+    if (reading.value != -999.0) print(reading);
+  }
+}
